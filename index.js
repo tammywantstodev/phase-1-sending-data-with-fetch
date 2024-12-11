cc06e4bde9cd4cd22c31eb54d9c381ab057ec43d
Tamara Mwanzali Kaka <billnyetherussianspy679@gmail.com>
2024-12-11 14:58:30 +0300

    pushing work

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(){
+    const body = document.querySelector('body');
+   return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com",
+        }),
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        const idElement = document.createElement('p');
+        idElement.textContent = `ID: ${data.id}`;
+        body.appendChild(idElement);
+    })
+    .catch(function (error) {
+       const errorElement = document.createElement('p');
+       errorElement.textContent=`Error: ${error.message}`;
+       body.appendChild(errorElement);
+    });
+
+
+}
